17bb4f610d070155c7ad441bb33cb84eab3f7a51
Anand-Theertha <54447234+Anand-Theertha@users.noreply.github.com>
2019-10-29 10:22:41 +0530

    Add files via upload

---
+++ Bitwise operators/Solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  //Write your code here.
+  int i,j; //used to represent a and b
+  int AND=0,OR=0,XOR=0; //setting max values of and,or and xor operators
+  for(i=1;i<n;i++) {
+      for(j=i+1;j<n;j++) {
+          if(((i&j)>AND) && ((i&j)<k))
+            AND=i&j;
+          if(((i|j)>OR) && ((i|j)<k))
+            OR=i|j;
+          if(((i^j)>XOR) && (i^j)<k)
+            XOR=i^j;
+        }
+    }
+    printf("%d\n%d\n%d",AND,OR,XOR);
+}
+
+ 
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
