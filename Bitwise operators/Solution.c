e38cb8f925cecb32658ea3742b20f66f1d03c270
Anand-Theertha <54447234+Anand-Theertha@users.noreply.github.com>
2019-10-29 10:35:30 +0530

    Update solution.c
    i<n and j<n does not satisfy 4/14 test cases. 
    Replaced with i<=n and j<=n

--- Bitwise operators/Solution.c
+++ Bitwise operators/Solution.c
@@ -9,8 +9,8 @@ void calculate_the_maximum(int n, int k) {
   //Write your code here.
   int i,j; //used to represent a and b
   int AND=0,OR=0,XOR=0; //setting max values of and,or and xor operators
-  for(i=1;i<n;i++) {
-      for(j=i+1;j<n;j++) {
+  for(i=1;i<=n;i++) {
+      for(j=i+1;j<=n;j++) {
           if(((i&j)>AND) && ((i&j)<k))
             AND=i&j;
           if(((i|j)>OR) && ((i|j)<k))
